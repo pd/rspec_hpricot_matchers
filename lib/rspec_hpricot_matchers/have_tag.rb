cbc0bd52f99fe19304bccad383694e92b8ee2c71
Kyle Hargraves <philodespotos@gmail.com>
2008-03-05 19:16:26 -0600

    make :matches? slightly less hideously large

--- lib/rspec_hpricot_matchers/have_tag.rb
+++ lib/rspec_hpricot_matchers/have_tag.rb
@@ -11,52 +11,23 @@ module RspecHpricotMatchers
       end
     end
 
-    def matches?(actual)
+    def matches?(actual, &block)
       @actual = actual
-      @hdoc = if Hpricot === @actual
-                @actual
-              elsif @actual.respond_to?(:body)
-                Hpricot(@actual.body)
-              else
-                Hpricot(@actual.to_s)
-              end
+      @hdoc = hdoc_for(@actual)
 
       matched_elements = @hdoc.search(@selector)
       return false if matched_elements.empty?
 
       if @inner_text
-        matched_elements = matched_elements.select do |e|
-          case @inner_text
-          when Regexp
-            e.inner_text =~ @inner_text
-          else
-            e.inner_text == @inner_text
-          end
-        end
+        matched_elements = filter_on_inner_text(matched_elements)
       end
 
-      if block_given?
-        matched_elements = matched_elements.select do |e|
-          begin
-            yield e
-          rescue Spec::Expectations::ExpectationNotMetError
-            false
-          else
-            true
-          end
-        end
+      if block
+        matched_elements = filter_on_nested_expectations(matched_elements, block)
       end
 
       @actual_count = matched_elements.length
-      if @options[:count]
-        return false unless @options[:count] === matched_elements.length
-      end
-      if @options[:minimum]
-        return false unless matched_elements.length >= @options[:minimum]
-      end
-      if @options[:maximum]
-        return false unless matched_elements.length <= @options[:maximum]
-      end
+      return false if not acceptable_count?(@actual_count)
 
       !matched_elements.empty?
     end
@@ -68,10 +39,52 @@ module RspecHpricotMatchers
 
     def negative_failure_message
       explanation = @actual_count ? "but found #{@actual_count}" : "but did"
-      "did not expect\n#{@hdoc.to_s}\nto have #{failure_count_phrase} #{failure_selector_phrase}, #{explanation}"
+      "expected\n#{@hdoc.to_s}\nnot to have #{failure_count_phrase} #{failure_selector_phrase}, #{explanation}"
     end
 
     private
+      def hdoc_for(input)
+        if Hpricot === input
+          input
+        elsif input.respond_to?(:body)
+          Hpricot(input.body)
+        else
+          Hpricot(input.to_s)
+        end
+      end
+
+      def filter_on_inner_text(elements)
+        elements.select do |el|
+          next(el.inner_text =~ @inner_text) if @inner_text.is_a?(Regexp)
+          el.inner_text == @inner_text
+        end
+      end
+
+      def filter_on_nested_expectations(elements, block)
+        elements.select do |el|
+          begin
+            block.call(el)
+          rescue Spec::Expectations::ExpectationNotMetError
+            false
+          else
+            true
+          end
+        end
+      end
+
+      def acceptable_count?(actual_count)
+        if @options[:count]
+          return false unless @options[:count] === actual_count
+        end
+        if @options[:minimum]
+          return false unless actual_count >= @options[:minimum]
+        end
+        if @options[:maximum]
+          return false unless actual_count <= @options[:maximum]
+        end
+        true
+      end
+
       def failure_count_phrase
         if @options[:count]
           "#{@options[:count]} elements matching"
